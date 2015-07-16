d7c69d7ce23febe4f5ee5a0c0e07865d71dca5fa
Vincent Rischmann <vincent@rischmann.fr>
2015-07-16 23:01:34 +0200

    Add tests that actually read from the environment variables

--- userdir_linux_test.go
+++ userdir_linux_test.go
@@ -3,6 +3,7 @@
 package userdir_test
 
 import (
+	"os"
 	"strings"
 	"testing"
 
@@ -15,7 +16,27 @@ func TestGetDataHome(t *testing.T) {
 	require.True(t, strings.HasSuffix(d, ".local/share"))
 }
 
+func TestGetDataHomeFromVariable(t *testing.T) {
+	tmp := os.Getenv("XDG_DATA_HOME")
+	os.Setenv("XDG_DATA_HOME", "/tmp/foo")
+
+	d := userdir.GetDataHome()
+	require.Equal(t, "/tmp/foo", d)
+
+	os.Setenv("XDG_DATA_HOME", tmp)
+}
+
 func TestGetConfigHome(t *testing.T) {
 	d := userdir.GetConfigHome()
 	require.True(t, strings.HasSuffix(d, ".config"))
+}
+
+func TestGetConfigHomeFromVariable(t *testing.T) {
+	tmp := os.Getenv("XDG_CONFIG_HOME")
+	os.Setenv("XDG_CONFIG_HOME", "/tmp/foo")
+
+	d := userdir.GetConfigHome()
+	require.Equal(t, "/tmp/foo", d)
+
+	os.Setenv("XDG_CONFIG_HOME", tmp)
 }
